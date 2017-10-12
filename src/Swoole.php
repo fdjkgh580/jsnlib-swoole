23a70c026c223d72776435b8433bcf5cae2d7160
fdjkgh580 <fdjkgh580@yahoo.com.tw>
2017-10-12 17:05:25 +0800

    add push_target()

--- src/Swoole.php
+++ src/Swoole.php
@@ -9,6 +9,7 @@ class Swoole
 	 * @param    int    $self         自己的連線編號
 	 * @param    bool   $is_send_self 是否要發送給自己？建議使用 false
 	 * @param    string $data         發送的數據
+	 * 
 	 */
 	static public function push_all($param = []) 
 	{
@@ -21,4 +22,26 @@ class Swoole
 			$param['ws']->push($fd, $param['data']);
 		}
 	}
+
+	/**
+	 * 發送給特定對象
+	 * @param    object $ws           Websocket 物件
+	 * @param    int    $self         自己的連線編號
+	 * @param    bool   $is_send_self 是否要發送給自己？建議使用 false
+	 * @param    array  $target       要發送的編號
+	 * @param    string $data         發送的數據
+	 * 
+	 */
+	static public function push_target($param = []) 
+	{
+		// 取出所有的連線編號
+		foreach ($param['target'] as $fd)
+		{
+			if ($fd == $param['self'] and $param['is_send_self'] === false) continue;
+
+			$param['ws']->push($fd, $param['data']);
+		}
+	}
+
+
 }
